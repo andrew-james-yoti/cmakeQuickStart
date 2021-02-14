74b2d0823765bd1e9ca037895da0d241f62d6354
andrew-james-yoti <andrew.james@yoti.com>
2021-02-14 15:11:56 +0000

    removed ignored files

--- build/HelloWorld.h
+++
@@ -1,3 +0,0 @@
-// the configured options and settings for HelloWorld
-#define HelloWorld_VERSION_MAJOR 1
-#define HelloWorld_VERSION_MINOR 0
--- build/googletest/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/jamesan/MyDevelopment/cplusplus/cmakeQuickStart/googletest
-# Build directory: /Users/jamesan/MyDevelopment/cplusplus/cmakeQuickStart/build/googletest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("googlemock")
